3aec55978c842e5661ae461ca40102543fa94656
CEP <galcera@hotmail.com>
2019-03-07 13:47:50 +0100

    Creation models

---
+++ app/Http/Controllers/SubtipusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subtipus;
+use Illuminate\Http\Request;
+
+class SubtipusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subtipus  $subtipus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subtipus $subtipus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subtipus  $subtipus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subtipus $subtipus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Subtipus  $subtipus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subtipus $subtipus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subtipus  $subtipus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subtipus $subtipus)
+    {
+        //
+    }
+}
