743ddf33a83598a819800d053b688f26c23dc54a
ElTitoBadi <artur.bcn1998@gmail.com>
2019-03-13 23:57:53 +0100

    Revert "Revert "login style update""
    This reverts commit ca2f2502438c245e3da6b53837e5c27fdc6eaa87.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,18 +11,21 @@
     <script src="{{ asset('js/libraries/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/libraries/popper.min.js') }}"></script>
     <script src="{{ asset('js/libraries/bootstrap.min.js') }}"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 </head>
 <body>
     <div class="loginBox">
-        <h4>Inicia sesión</h4>
+        <h3>Inicia sesión</h3>
         <form>
                 <div class="form-group">
-                    <label id="EmailLabel" for="Email">Correo electronico</label>
+                    <p id="EmailLabel">Correo electronico</p>
                     <input type="email" class="fill" id="Email">
                 </div>
                 <div class="form-group">
-                    <label id="PasswordLabel" for="Password">Contraseña</label>
+                    <p id="PasswordLabel">Contraseña</p>
                     <input type="password" class="fill" id="Password" >
+                    <div id="hideShow">visibility_off</div>
                 </div>
                 <div class="form-group d-flex" style="justify-content:flex-end">
                     <button type="submit" class="btn-primary button">Entrar</button>
@@ -32,12 +35,43 @@
 </body>
 
 <script>
+    var hide = true;
     $(".fill").on('focus',function(){
-        $("#" + $(this).attr("id") + "Label").css("color","var(--primary-color-dark)");
+        var $label = $("#" + $(this).attr("id") + "Label");
+        $label.css({color: "var(--primary-color-dark)"});
+        $label.animate({"top":"4px",
+                        "font-size":"13px",
+                        "z-index":"0"}, 120);
     });
     $(".fill").on('blur',function(){
-        $("#" + $(this).attr("id") + "Label").css("color","#8b8b8b");
+        var $label = $("#" + $(this).attr("id") + "Label");
+        if(!$("#Password").is(":focus")){
+        if($(this).val() == ""){
+            $label.animate({"top":"15px",
+                            "font-size":"17px",
+                            "z-index":"-1"},120);
+        }
+        $label.css({color: "#8a8a8a"});
+        }
     });
-</script>
+
+    $("#hideShow").click(function(e){
+        var $this = $(this);
+        var $input = $("#Password");
+
+        if(hide){
+            hide=false;
+            $this.text("visibility");
+            $this.css("color", "var(--primary-color-dark)")
+            $input.attr("type", "text");
+        }else{
+            hide=true;
+            $this.text("visibility_off");
+            $this.css("color", "#8a8a8a6e")
+            $input.attr("type", "password");
+        }
+
+    });
+        </script>
 </html>
 
