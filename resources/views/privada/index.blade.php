5713ed112f89172899d62dadd894e89c84b85799
ElTitoBadi <artur.bcn1998@gmail.com>
2019-03-17 00:48:04 +0100

    template part privada

--- resources/views/privada/index.blade.php
+++ resources/views/privada/index.blade.php
@@ -1,5 +1,5 @@
 @extends('privada.templates.master')
 
 @section('body')
-PENE
+
 @endsection
--- resources/views/privada/templates/master.blade.php
+++ resources/views/privada/templates/master.blade.php
@@ -8,28 +8,80 @@
     <title>@yield('titulo')</title>
     <link rel="stylesheet" href="{{ asset('css/libraries/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/root.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/menu-mobil.css') }}">
     <script src="{{ asset('js/libraries/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/libraries/popper.min.js') }}"></script>
-    <script src="{{ asset('js/libraries/bootstrap.min.js') }}"></script>    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+    <script src="{{ asset('js/libraries/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/eventsMasterPrivada.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
 </head>
 
 <body>
+
+
     <nav id="menuSuperior">
         <img height="70%" src="{{ asset('media/img/logo_spam_full.png') }}" alt="">
     </nav>
     <div id="logo">
         <img height="70%" src="{{ asset('media/img/artio.png') }}" alt="">
+        <div class="menu-btn">
+            <div class="menu-btn-animation"></div>
+            <div class="menu-btn-animation"></div>
+            <div class="menu-btn-animation"></div>
+        </div>
+    </div>
+    <div class="menu">
+        <div class="menu-list">
+            <ul>
+                <li>
+                    <a href="">
+                        <p>Donacions</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <p>Donants</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <p>Usuaris</p>
+                    </a>
+                </li>
+                <li class="mt-5">
+                    <a href="">
+                        <p>Sortir</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="menu-close-space">
+
+        </div>
     </div>
     <nav id="menuLateral">
         <ul id="opcionsMenuLateral">
-            <li>
-                <img class="mb-1" width="40px" src="{{ asset('media/img/donacio.png') }}" alt="">Donacions</li>
-            <li>
-                <img class="mb-1" width="40px" src="{{ asset('media/img/donant.png') }}" alt="">Donants</li>
-            <li>
-                <img class="mb-1" width="40px" src="{{ asset('media/img/usuari.png') }}" alt="">Usuaris</li>
-            <li>
-                <img class="mb-1" width="40px" src="{{ asset('media/img/exit.png') }}" alt="">Sortir</li>
+            <li class="active">
+                <a href="">
+                    <img class="mb-1" width="40px" src="{{ asset('media/img/donacio.png') }}" alt="">
+                    Donacions
+                </a>
+            </li>
+            <li><a href="">
+                    <img class="mb-1" width="40px" src="{{ asset('media/img/donant.png') }}" alt="">
+                    Donants
+                </a>
+            </li>
+            <li><a href="">
+                    <img class="mb-1" width="40px" src="{{ asset('media/img/usuari.png') }}" alt="">
+                    Usuaris
+                </a>
+            </li>
+            <li><a href="">
+                    <img class="mb-1" width="40px" src="{{ asset('media/img/exit.png') }}" alt="">
+                    Sortir
+                </a>
+            </li>
         </ul>
     </nav>
     @yield('body')
