d5af7ca4ed2ccf78d908227b602454cb6e7cb961
ElTitoBadi <artur.bcn1998@gmail.com>
2019-03-30 01:22:13 +0100

    -

--- public/js/eventsCrearDonacio.js
+++ public/js/eventsCrearDonacio.js
@@ -1,14 +1,62 @@
-function getDonant(dni){
+var donante = false;
+$(document).ready(function () {
+    $('#btnAnonim').click(function () {
+        ocultarBotones($(this).attr('id'));
+    });
+    $('#btnParticular').click(function () {
+        $('#modalParticular').modal();
+    });
+    $('#btnEmpresa').click(function () {
+        $('#modalEmpresa').modal();
+    });
+});
+
+$('#modalParticular .btn').click(function () {
+    var $this = $(this);
+    var dni = $('#modalParticular input').val();
+    if (dni == '') {
+
+    }
+    else {
+        $('.spinner-border').show();
+        $this.hide();
+        getDonant(dni, function (data) {
+            if (data != '') {
+                console.log(data)
+            }
+            else {
+                alert('bad');
+            }
+            $('.spinner-border').show();
+            $this.hide();
+        });
+    }
+});
+
+function getDonant(dni, callback) {
     $.ajax({
         async: 'true',
         method: 'GET',
         url: 'http://localhost:8080/spam-abp/public/api/donants/' + dni,
-        success: function(data){
-            log(data)
+        success: function (data) {
+            callback(data);
         }
     })
 }
 
-function setDonant(data){
+function setDonant(data) {
 
+}
+
+function ocultarBotones(id) {
+    $('.btn-donant').hide();
+    $('#' + id).show();
+    $('#' + id).addClass('closeBtn');
+    $('#' + id).click(function () {
+        $('#' + id).click(function () {
+            ocultarBotones($(this).attr('id'));
+        });
+        $('.btn-donant').show();
+        $('#' + id).removeClass('closeBtn');
+    });
 }
--- resources/views/privada/crearDonacion.blade.php
+++ resources/views/privada/crearDonacion.blade.php
@@ -11,25 +11,68 @@
         <h3>Selecciona un tipus de donant</h3>
         <div class="buttons">
             <button id="btnParticular" class="btn-donant">
-                <img src="{{ asset('media/img/user.png') }}" alt="">
                 <p>Particular</p>
             </button>
             <button id="btnEmpresa" class="btn-donant">
-                <img src="{{ asset('media/img/empresa.png') }}" alt="">
                 <p>Empresa</p>
             </button>
             <button id="btnAnonim" class="btn-donant">
-                <img src="{{ asset('media/img/anonim.png') }}" alt="">
                 <p>Anònim</p>
             </button>
             <form action="{{ action('DonanteController@create') }}" method="get">
                 <input type="hidden" name="back">
                 <button id="btnNou" class="btn-donant">
-                    <img src="{{ asset('media/img/plus.png') }}" alt="">
                     <p>Nou</p>
                 </button>
             </form>
         </div>
     </div>
+
+    <div class="modal fade" id="modalParticular" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">DNI del particular</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <input id="dni" type="text" class="form-control" maxlength="9">
+                </div>
+                <button id="cargaDni" type="button" class="btn btn-primary float-right">Cargar donante</button>
+                <div class="spinner-border float-right d-none mr-2" role="status">
+                        <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="modal fade" id="modalEmpresa" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">CIF de la empresa</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" maxlength="9">
+                        <small class="form-text text-muted"></small>
+                    </div>
+                    <button type="button" class="btn btn-primary float-right">Cargar donante</button>
+                    <div class="spinner-border float-right d-none mr-2" role="status">
+                            <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
 @endsection
 
