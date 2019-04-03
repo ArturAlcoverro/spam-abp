f858f1bbee75c6cd0bf7cf0004d3e84f70db6815
CEP <galcera@hotmail.com>
2019-04-03 13:40:10 +0200

    -

--- public/js/eventsCrearDonacio.js
+++ public/js/eventsCrearDonacio.js
@@ -1,31 +1,52 @@
+var infoDonante = true;
 var donante;
 var isDonante = false;
 $(document).ready(function () {
+    // l'usuari seleccionas és anonim
     $('#btnAnonim').click(function () {
         ocultarBotones('btnAnonim');
         isDonante = true;
+        $('.showHide').show();
     });
+
+    // Obre el modal per buscar un particular
     $('#btnParticular').click(function () {
         $('.not-found').addClass('d-none');
         $('.empty-error').addClass('d-none');
         $('#modalParticular').modal();
     });
+
+    // Obre el modal per buscar una empresa
     $('#btnEmpresa').click(function () {
         $('.not-found').addClass('d-none');
         $('.empty-error').addClass('d-none');
         $('#modalEmpresa').modal();
     });
+
+    // Obre el modal per agregar una donació de material
     $('#btnMaterial').click(function () {
         $('#modalMaterial').modal();
     });
 
+    // Obre el modal per agregar una donació de diners
+    $('#btnDiners').click(function () {
+        $('#modalDiners').modal();
+    });
+
+    // Eliminar el usuario seleccionado
     $('.closeBtn').click(function () {
         $('#titleDonant').text('Selecciona un donant');
         $('.btn-donant').show();
         $('.closeBtn').hide();
         $("#infoParticular").hide();
         $("#infoEmpresa").hide();
+        $(".showHide").hide();
+
         isDonante = false;
+    });
+
+    $('.showHide').click(function(){
+        toggleDonant();
     });
 
     $('#modalParticular .btn').click(function () {
@@ -92,8 +113,16 @@ $(document).ready(function () {
     });
 
     $('#formMaterial').submit(function (event) {
-        alert("submit");
         event.preventDefault();
+        var data = ($(this).serializeArray());
+        console.log(data);
+    });
+
+    $('#tablaDonacions').DataTable({
+        responsive: true,
+        sort: false,
+        dom: 'rt',
+        select: false,
     });
 
 
@@ -110,10 +139,22 @@ function getDonant(dni, callback) {
     })
 }
 
+function toggleDonant(){
+    if(infoDonante){
+        $('#donante').hide(100);
+        $('.showHide').addClass('rotate-180');
+        infoDonante = false;
+    }
+    else{
+        $('#donante').show(100);
+        $('.showHide').removeClass('rotate-180');
+        infoDonante = true;
+    }
+}
+
 function setDonant(data) {
     var habitual;
     var nombre = data.nombre;
-
     if (data.tipos_donantes_id == 1) {
         ocultarBotones('btnEmpresa');
         $("#infoEmpresa #nombreEmpresa span").text(nombre.charAt(0).toUpperCase() + nombre.slice(1));
@@ -125,6 +166,7 @@ function setDonant(data) {
         $("#infoParticular #dniParticular span").text(data.cif);
         $("#infoParticular").show();
     }
+    $(".showHide").show();
 }
 
 function ocultarBotones(id) {
