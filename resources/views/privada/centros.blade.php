bfd49acece22df9c2fb2646b8ff520aaaade08aa
CEP <galcera@hotmail.com>
2019-04-11 09:43:18 +0200

    centrO fix

--- resources/views/privada/centros.blade.php
+++ resources/views/privada/centros.blade.php
@@ -66,7 +66,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header border-0 pb-0">
-                    <h5 class="modal-title" id="exampleModalLabel">Editar centrO</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Editar centro</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
