e7cc74b541b632b7b5271ae71e49e5bfe52405fd
davicorpe <davicorpe23@gmail.com>
2019-04-21 21:19:39 +0200

    traducciones3

---
+++ resources/lang/en/home.php
@@ -0,0 +1,13 @@
+<?php
+    return[
+        "graphics"      =>"Estadisticas",
+        "text1"         =>"¿Puede un animal sin hogar disfrutar tanto como tu mascota?",
+        "text2"         =>"¡Nosotros así lo creemos!",
+        "text3"         =>"Cada dia en los centros de acogida se cuidan innumerables animales y se gasta una gran cantidad de recursos",
+        "text4"         =>"Recursos como:",
+        "food"          =>"Comida",
+        "medicines"     =>"Medicinas",
+        "higiene"       =>"Higiene",
+        "text5"         =>"¡Contamos con tu ayuda!"
+    ];
+?>
--- resources/views/publica/templates/main.blade.php
+++ resources/views/publica/templates/main.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="{{app()->getLocale()}}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,14 +15,14 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="{{url('/')}}"><img class="" src="{{ asset('media/img/logo_spam.png') }}" alt="" style="width: 128px"></a>
+        <a class="navbar-brand" href="{{url('/')}}"><img class="" src="{{ asset('media/img/logo_spam.png') }}" alt="Ir al indice" style="width: 128px"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{url('/charts')}}">Estadísticas</a>
+                    <a class="nav-link" href="{{url('/charts')}}">@lang('home.graphics')</a>
                 </li>
             </ul>
         </div>
