c50cb65e6fbd3f69cc43ce42c1ab184dc1889475
CEP <galcera@hotmail.com>
2019-04-24 18:49:14 +0200

    -

--- resources/views/publica/charts.blade.php
+++ resources/views/publica/charts.blade.php
@@ -1,4 +1,10 @@
-@extends('publica.templates.main') @section('name') ARTIO @endsection @section('body')
+@extends('publica.templates.main')
+
+@section('name')
+ARTIO
+@endsection
+
+@section('body')
 <link rel="stylesheet" href="{{ asset('css/public-navbar.css') }}">
 <link rel="stylesheet" href="{{ asset('css/charts.css') }}">
 
@@ -7,41 +13,45 @@
     <div class="p-2">
         <div class="row grafics-card" style="height: calc(100vh - 92px);">
 
-                <div class="col-3 py-2 pl-2 pr-0">
-                    <div class="nav flex-column nav-pills mt-2" id="llistaOpcions" role="tablist" aria-orientation="vertical"></div>
+            <div class="col-3 py-2 pl-2 pr-0 tabs">
+                <div class="nav flex-column nav-pills mt-2" id="llistaOpcions" role="tablist" aria-orientation="vertical"></div>
 
-                </div>
+            </div>
 
-                <div class="col-9 p-2">
+            <div class="col-9 p-2 grafics">
 
-                    {{--
-                    <form action="" class="mt-2" method="POST">
-                        @csrf
+                {{--
+                <form action="" class="mt-2" method="POST">
+                    @csrf
 
-                        <div class="form-group row">
-                            <label for="dataInit" class="col-2 col-form-label">Data inicial</label>
-                            <div class="col-4">
-                                <input type="date" name="dataInit" id="dataInit" class="form-control">
-                            </div>
-
-                            <label for="dataFin" class="col-2 col-form-label">Data final</label>
-                            <div class="col-4">
-                                <input type="date" name="dataFin" id="dataFin" class="form-control">
-                            </div>
+                    <div class="form-group row">
+                        <label for="dataInit" class="col-2 col-form-label">Data inicial</label>
+                        <div class="col-4">
+                            <input type="date" name="dataInit" id="dataInit" class="form-control">
                         </div>
-                        <div class="form-group float-right">
-                            <button type="submit" name="actualizar" class="btn btn-primary boton-amplada">Actualitzar gràfic</button>
+
+                        <label for="dataFin" class="col-2 col-form-label">Data final</label>
+                        <div class="col-4">
+                            <input type="date" name="dataFin" id="dataFin" class="form-control">
                         </div>
-                    </form> --}}
+                    </div>
+                    <div class="form-group float-right">
+                        <button type="submit" name="actualizar" class="btn btn-primary boton-amplada">Actualitzar gràfic</button>
+                    </div>
+                </form> --}}
 
 
-                    <div class="tab-content" id="tabsGrafiques"></div>
-                </div>
+                <div class="tab-content" id="tabsGrafiques"></div>
+            </div>
 
         </div>
     </div>
 </div>
-@endsection @section('scripts')
+
+@endsection
+
+@section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
 <script src="{{ asset('js/functionsCharts.js') }}"></script>
-<script src="{{ asset('js/eventsCharts.js') }}"></script> @endsection
+<script src="{{ asset('js/eventsCharts.js') }}"></script>
+@endsection
