babe8f35140f5055d0e386f709e4121d62fb8299
CEP <galcera@hotmail.com>
2019-04-24 13:44:31 +0200

    Video Finish

--- resources/lang/en/video.php
+++ resources/lang/en/video.php
@@ -1,5 +1,9 @@
 <?php
     return[
-        "videoFail"     =>"Your browser does not support the video tag."
+        "title"         =>"Meet the Invisibles",
+        "videoFail"     =>"Your browser does not support the video tag.",
+        "retBtn"         =>"Return to Video",
+        "thxLbl"        =>"Thanks for watch the video!",
+        "qq"            =>"Question"
     ];
 ?>
