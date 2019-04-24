a8630d3022e17753ff715d34100f0760cdbe5185
CEP <galcera@hotmail.com>
2019-04-24 14:10:58 +0200

    -

--- resources/views/publica/video.blade.php
+++ resources/views/publica/video.blade.php
@@ -9,13 +9,13 @@
 <link rel="stylesheet" href="{{ asset('css/public-navbar.css') }}">
 
 <div id="vcontainer" class="container" style="min-height: 100vh">
-    <h2 class="text-center font-weight-light mt-3 delighted-text text-white mb-2">@lang('video.title')</h2>
+    <h1 class="  font-weight-light mt-5 delighted-text mb-2">@lang('video.title')</h1>
     <video width="720" height="480" controls class="mx-auto mt-4" style="width: 100%; height: auto">
         <source src="{{ asset('media/publica/SPAM_DEFDEF.mp4') }}" type="video/mp4">
         @lang('video.videoFail')
     </video>
     <h3 class="hidden-video text-center font-weight-light mt-5 delighted-text">@lang('video.qq')</h3>
-    <h4 id="pregunta" class="hidden-video text-center font-weight-light mt-5 delighted-text">¿Cuántos perros de razas potencialmente peligrosas crees que SPAM acogió en este último año?</h4>
+    <h4 id="pregunta" class="hidden-video text-center font-weight-light mt-5">¿Cuántos perros de razas potencialmente peligrosas crees que SPAM acogió en este último año?</h4>
     <div class="row my-5">
 
     </div>
